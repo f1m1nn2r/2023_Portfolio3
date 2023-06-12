0a4b16e773ef2bd6f365537924d4f58ae60754b6
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-06-12 21:38:20 +0900

    2023.06.12_21:38

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+// 헤더 메뉴 클릭 시 섹션 이동
+const menu = document.querySelector('.at-header ul');
+menu.addEventListener('click', (e) => {
+    // 클릭한 메뉴의 dataset 값을 구하고
+    const thisMenu = e.target;
+    const menuValue = thisMenu.dataset.menu;
+    // 구한 dataset 값의 선택자를 찾아감
+    const scrollMenu = document.querySelector(menuValue);
+    scrollMenu.scrollIntoView({
+        behavior:'smooth'
+    });
+});
+
+// work 섹션 작업물 div 및 img 추가
 const stickyBox = document.querySelector('.sticky-box');
 for(var i=0; i<15; i++){
     const newDiv = document.createElement('div');
@@ -7,7 +21,7 @@ for(var i=0; i<15; i++){
 const workDiv = document.querySelectorAll('.work-sticky');
 for(var i=0; i<workDiv.length; i++){
     workDiv[i].classList.add(`work-sticky-${i+1}`);
-    workDiv[i].style.backgroundImage = `url('./img/work-${i+1}.PNG')`;
+    workDiv[i].style.backgroundImage = `url('./img/work-${i+1}.png')`;
     workDiv[i].style.backgroundSize = 'contain';
 }
 
@@ -21,12 +35,13 @@ window.addEventListener('scroll', () => {
     const RLbanner = document.querySelectorAll('.rl-banner-inner');
     for(var i=0; i<RLbanner.length; i++){
         if(i % 2 == 0){
-            RLbanner[i].style.transform = `translate3d(${-window.scrollY / document.querySelector('.float-txt').clientHeight}rem, 0, 0)`
+            RLbanner[i].style.transform = `translate3d(${-window.scrollY / document.querySelector('.at-header').clientHeight}rem, 0, 0)`
         }else if(i % 2 == 1){
             RLbanner[i].style.transform = `translate3d(${window.scrollY / (window.innerHeight)}rem, 0, 0)`
         }
     }
 });
+
 window.addEventListener('mousemove', (e) => {
     // 첫, 마지막 섹션 플로팅 텍스트 마우스 오버 효과
     var moveValueX = e.clientX / 20;
@@ -34,5 +49,13 @@ window.addEventListener('mousemove', (e) => {
     const float = document.querySelectorAll('.float-txt');
     for(var i=0; i<float.length; i++){
         float[i].style.transform = `translate3d(${moveValueX}px, ${moveValueY}px, 0) rotate(${(e.clientX / 10) / -document.querySelector('.at-header').scrollHeight}deg)`
+    }
+});
+
+window.addEventListener('mousewheel', (e) => {
+    if(e.deltaY > 0 && window.scrollY > document.querySelector('.section01').clientHeight){
+        document.querySelector('.at-header').classList.add('hide');
+    }else{
+        document.querySelector('.at-header').classList.remove('hide');
     }
 });
